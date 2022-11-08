0c6364ba753ad7e3d49fd70b002eb87bc4c40579
Nikhil Roy <nikhilchandraroypoet@gmail.com>
2022-11-08 22:48:24 +0600

    update custom_dropdown.html

--- js/custom_dropdown/custom_dropdown.js
+++ js/custom_dropdown/custom_dropdown.js
@@ -4,7 +4,6 @@ function CustomDropdown(_selectTag) {
     // below working
     let dropdown_select = $select('.custom_nl_dropdown select');
     //dropdown_select
-    let dropdown_list = $selectAll('.custom_nl_dropdown select option');
     let dropdown_ul = document.createElement('ul');
 
 
@@ -14,22 +13,26 @@ function CustomDropdown(_selectTag) {
         // dropdown_ul.remove(); // remove dropdown list
         $select('.dropdown_select_action').classList.remove('dropdown_select_action'); // remove class for default
     } else {
+        let dropdown_list = _selectTag.querySelectorAll('select option')
         dropdown_ul.classList.add('dropdown_ul')
         //console.log(_selectTag.nextEle)
         //console.log(_selectTag.classList.contains('dropdown_select_action'))
         if (_selectTag.classList.contains('dropdown_select_action') === false) {
             _selectTag.parentNode.appendChild(dropdown_ul)
         }
-        _selectTag.classList.add('dropdown_select_action')
-        dropdown_list.forEach(v => {
-            dropdown_ul.innerHTML += `<li data-value="${v.value}"> ${v.innerHTML} </li>`
+        _selectTag.classList.add('dropdown_select_action');
+
+        dropdown_list.forEach((v, i) => {
+            dropdown_ul.innerHTML += `<li class="${_selectTag.querySelector('select').value === v.value ? 'list_active_hover' : ''}" 
+            onmouseover="dropdownItemOver(this)" onclick="dropdownItemFunc('${v.innerHTML}', '${v.value}', this)" data-value="${v.value}"> ${v.innerHTML} </li>`
         })
     }
 }
 
 // calling function
 $selectAll('.custom_nl_dropdown .select_wrapper').forEach(v => {
-    v.onclick = () => {
+    v.onclick = function (j) {
+        console.log(v)
         CustomDropdown(v);
         document.addEventListener('mousedown', CustomDropdownClose);
     }
@@ -37,9 +40,31 @@ $selectAll('.custom_nl_dropdown .select_wrapper').forEach(v => {
 
 
 function CustomDropdownClose(e) {
-    if (!$select('.dropdown_ul').contains(e.target)) {
-        //box.classList.add('hide')
-        // alert('hide')
-        CustomDropdown('hide');
+    if (Boolean($select('.dropdown_ul'))) {
+        if (!$select('.dropdown_ul').contains(e.target)) {
+            //box.classList.add('hide')
+            // alert('hide')
+            CustomDropdown('hide');
+        }
     }
+}
+
+function dropdownItemFunc(_text, _value, _self) {
+    //alert('hi')
+    //console.log($select('.dropdown_select_action select'))
+    //_selectTag.classList.add('dropdown_action_done');
+    _self.parentElement.previousElementSibling.classList.add('dropdown_action_done')
+
+    $select('.dropdown_select_action select').style.fontWeight = 'bold';
+    $select('.dropdown_select_action select').value = _value;
+    CustomDropdown('hide');
+}
+
+
+function dropdownItemOver(_self) {
+    let allList = _self.parentElement.querySelectorAll('li');
+    allList.forEach(v => {
+        v.classList.remove('list_active_hover');
+    })
+    _self.classList.add('list_active_hover')
 }
